8d2f281a2aa18923f1c738a06861210c305b5d50
Robert Evans <robertevans@robertevansmb.com>
2023-05-11 13:14:15 -0400

    Reviews length

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -22,6 +22,9 @@ export default function RestaurantCard({restaurant}: Props) {
                 <div className="p-1">
                     <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
                     <div className="flex items-start">
+                        <div>
+                            {restaurant.reviews.length}
+                        </div>
                         <!--
                         <Stars reviews={restaurant.reviews} />
                         <p className="ml-2">{restaurant.reviews.length} review{restaurant.reviews.length === 1 ? "":"s"}</p>
