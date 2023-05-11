84e84b04cb99e126a1ce1887726b256a064bcd99
Robert Evans <robertevans@robertevansmb.com>
2023-05-11 13:12:59 -0400

    Remove section temporarily

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -22,8 +22,10 @@ export default function RestaurantCard({restaurant}: Props) {
                 <div className="p-1">
                     <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
                     <div className="flex items-start">
+                        <!--
                         <Stars reviews={restaurant.reviews} />
                         <p className="ml-2">{restaurant.reviews.length} review{restaurant.reviews.length === 1 ? "":"s"}</p>
+                        -->
                     </div>
                     <div className="flex text-reg font-light capitalize">
                         <p className=" mr-3">{restaurant.cuisine.name}</p>
